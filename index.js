786aae4348faa929f985df475448793c31bcb79c
amosmaundundu <amosmaundu5@gmail.com>
2022-08-24 21:06:50 +0300

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+ const tutorials =[
+  "What Does The This Keyword Mean?",
+  "What Is The Constructor OO Pattern?",
+  "Implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What Is NaN And How Can We Check For It",
+  "What Is The Difference Between StopPropagation And PreventDefault?",
+  "Immutable State And Pure Functions",
+  "What Is The Difference Between == And ===?",
+  "What Is The Difference Between Event Capturing And Bubbling?",
+  "What Is JSONP?"
 ];
+
+
+
+
 
 const titleCased = () => {
   return tutorials
 }
+
+  let map1 =tutorials.map((items)=>{
+
+    let words =items.split(" ");
+    for(let i=0;i<words.length;i++){
+       words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+     return words.join(" ");
+    }
+
+
+});{
+
+ map1;
+};
